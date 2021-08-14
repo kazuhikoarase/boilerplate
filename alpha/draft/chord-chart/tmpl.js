9691135cb7ea815d9567ba73ca79c7827b8f37e3
kazuhikoarase <kazuhiko.arase@gmail.com>
2021-08-15 05:22:11 +0900

    commit

---
+++ alpha/draft/chord-chart/tmpl.js
@@ -0,0 +1,16 @@
+
+'use strict'
+
+window.addEventListener('load', function() {
+
+  var $s = svgtk.$s;
+  var pathBuilder = svgtk.pathBuilder;
+
+  var width = 400;
+  var height = 300;
+
+  var svg = svgtk.create(width, height, true);
+
+  // TODO
+
+});
